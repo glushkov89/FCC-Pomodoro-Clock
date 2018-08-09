5ecbd478a57f82e3209cf726d95cf7f2def370e7
glushkov89 <kellerman117@gmail.com>
2018-08-09 15:41:01 +0300

    Basic layout for mobile.

--- src/containers/controls.js
+++ src/containers/controls.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import FontAwesome from "react-fontawesome";
+import FontAwesomeIcon from "react-fontawesome";
 
 //Components
 //import Timer from "../components/timer";
@@ -88,50 +88,118 @@ class Controls extends Component {
 	// };
 
 	render() {
+		//	console.log(this.state);
 		return (
 			<div id="controls">
-				<div id="control-playback-group">
-					<button onClick={this.playPause} id="start_stop">
-						<FontAwesome name="fas fa-play" />
-						<FontAwesome name="fas fa-pause" />
-					</button>
-					<button onClick={this.stop} id="reset">
-						<FontAwesome name="fas fa-stop" />
-					</button>
-					<button onClick={this.resetControls} id="reset">
-						<FontAwesome name="fas fa-undo" />
-					</button>
+				<div id="clock-container">
+					<Timer
+						{...this.state}
+						// {...this.controls}
+					/>
+					<div id="time-input">
+						<div id="control-break-group">
+							<div id="break-label">Break</div>
+							<div onClick={this.incrBreak} id="break-increment">
+								{this.state.break === 60 ? (
+									<FontAwesomeIcon name="fas fa-minus" />
+								) : (
+									<FontAwesomeIcon name="fas fa-angle-up" />
+								)}
+							</div>
+							<div id="break-length">{this.state.break}</div>
+							<div onClick={this.decrBreak} id="break-decrement">
+								{this.state.break === 1 ? (
+									<FontAwesomeIcon name="fas fa-minus" />
+								) : (
+									<FontAwesomeIcon name="fas fa-angle-down" />
+								)}
+							</div>
+						</div>
+						<div id="control-session-group">
+							<div id="session-label">Session</div>
+							<div onClick={this.incrSession} id="session-increment">
+								{this.state.session === 60 ? (
+									<FontAwesomeIcon name="fas fa-minus" />
+								) : (
+									<FontAwesomeIcon name="fas fa-angle-up" />
+								)}
+							</div>
+							<div id="session-length">{this.state.session}</div>
+							<div onClick={this.decrSession} id="session-decrement">
+								{this.state.session === 1 ? null : (
+									<FontAwesomeIcon name="fas fa-angle-down" />
+								)}
+							</div>
+						</div>
+					</div>
+					<div id="control-playback-group">
+						<div onClick={this.playPause} id="start_stop">
+							<FontAwesomeIcon name="fas fa-play" />
+							<FontAwesomeIcon name="fas fa-pause" />
+						</div>
+						<div onClick={this.stop} id="stop">
+							<FontAwesomeIcon name="fas fa-stop" />
+						</div>
+						<div onClick={this.resetControls} id="reset">
+							<FontAwesomeIcon name="fas fa-undo" />
+						</div>
+					</div>
 				</div>
-
-				<div id="control-break-group">
-					<div id="break-label">Break Length</div>
-					<button onClick={this.incrBreak} id="break-increment">
-						<FontAwesome name="fas fa-angle-up" />
-					</button>
-					<div id="break-length">{this.state.break}</div>
-					<button onClick={this.decrBreak} id="break-decrement">
-						<FontAwesome name="fas fa-angle-down" />
-					</button>
+				<div id="my-footer">
+					<a
+						href="https://github.com/glushkov89/FCC-Pomodoro-Clock"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon name="fas fa-github" />
+					</a>
 				</div>
-
-				<div id="control-session-group">
-					<div id="session-label">Session Length</div>
-					<button onClick={this.incrSession} id="session-increment">
-						<FontAwesome name="fas fa-angle-up" />
-					</button>
-					<div id="session-length">{this.state.session}</div>
-					<button onClick={this.decrSession} id="session-decrement">
-						<FontAwesome name="fas fa-angle-down" />
-					</button>
-				</div>
-
-				<Timer
-					{...this.state}
-					// {...this.controls}
-				/>
 			</div>
 		);
 	}
 }
 
 export default Controls;
+
+/* 
+ (<FontAwesomeIcon name="fas fa-minus" />) 
+
+<div id="controls">
+				<div id="control-playback-group">
+					<div onClick={this.playPause} id="start_stop">
+						<FontAwesomeIcon name="fas fa-play" />
+						<FontAwesomeIcon name="fas fa-pause" />
+					</div>
+					<div onClick={this.stop} id="reset">
+						<FontAwesomeIcon name="fas fa-stop" />
+					</div>
+					<div onClick={this.resetControls} id="reset">
+						<FontAwesomeIcon name="fas fa-undo" />
+					</div>
+				</div>
+
+				<div id="control-break-group">
+					<div id="break-label">Break Length</div>
+					<div onClick={this.incrBreak} id="break-increment">
+						<FontAwesomeIcon name="fas fa-angle-up" />
+					</div>
+					<div id="break-length">{this.state.break}</div>
+					<div onClick={this.decrBreak} id="break-decrement">
+						<FontAwesomeIcon name="fas fa-angle-down" />
+					</div>
+				</div>
+				<Timer
+					{...this.state}
+					// {...this.controls}
+				/>
+				<div id="control-session-group">
+					<div id="session-label">Session Length</div>
+					<div onClick={this.incrSession} id="session-increment">
+						<FontAwesomeIcon name="fas fa-angle-up" />
+					</div>
+					<div id="session-length">{this.state.session}</div>
+					<div onClick={this.decrSession} id="session-decrement">
+						<FontAwesomeIcon name="fas fa-angle-down" />
+					</div>
+				</div>
+			</div> */
